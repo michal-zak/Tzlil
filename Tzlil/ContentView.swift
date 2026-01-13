d5059585c2f1d66c83b209c82a968fa2124e35fd
michal-zak <michalp752@gmail.com>
2026-01-13 19:26:59 +0100

    Initial Commit

---
+++ Tzlil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tzlil
+//
+//  Created by user945522 on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tzlil/TzlilApp.swift
@@ -0,0 +1,17 @@
+//
+//  TzlilApp.swift
+//  Tzlil
+//
+//  Created by user945522 on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct TzlilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
